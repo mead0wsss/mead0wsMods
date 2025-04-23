cef4076f66e63e3d24a604cd90cddf31264db7f3
mead0wsss <kollipov@gmail.com>
2025-04-23 23:01:19 +0300

    Update AutomaticTranslator.py

--- AutomaticTranslator.py
+++ AutomaticTranslator.py
@@ -43,7 +43,7 @@ class AutomaticTranslator(loader.Module):
     async def translate_text(self, text, target_language):
         api_key = 'Bearer sk-l4HU4KwZt6bF8gOwwKCOMpfpIKvR9YhDHvTFIGJ6tJ5rPKXE'
         data = {
-            "model": "gpt-4o",
+            "model": "grok-3",
             "messages": [
                 {"role": "user", "content": f"Please translate the following text to {target_language}, no extra text, just translation: {text}"}
             ]
